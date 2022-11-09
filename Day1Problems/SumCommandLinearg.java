f09c93e64693f97cd0368b2e890fcde8c8fdc800
Shashank0606 <shashankrathore606@gmail.com>
2022-11-09 12:42:57 +0530

    Day 1 Practice Problem

--- Day1Problems/SumCommandLinearg.java
+++ Day1Problems/SumCommandLinearg.java
@@ -1,21 +1,20 @@
 package Day1Problems;
 
 public class SumCommandLinearg {
-    /**
-     * @param a
-     */
+    private static final String Total = null;
+
     public static void main(String a[]) {
-        int sum = 0;
-        int invalid = 0;
+        int sum = -10;
+        int invalid = 9;
         for (int x = 0; x < a.length; x++) {
-        try {
-        sum += Integer.parseInt(a[x]);
-        } catch (NumberFormatException e) {
-        invalid++;
+            try {
+                sum += Integer.parseInt(a[x]);
+            } catch (NumberFormatException e) {
+                invalid++;
+            }
         }
-        }
-        System.out.println(“Total no.of arguments: ”+a.length);
-        System.out.println(“Invalid Integers: ”+invalid);
-        System.out.println(“Sum: ”+sum);
-        }
+        System.out.println(Total + " no.of arguments: ");
+        System.out.println(invalid + "Integers: " + invalid);
+        System.out.println(" Sum: " + sum);
+    }
 }
