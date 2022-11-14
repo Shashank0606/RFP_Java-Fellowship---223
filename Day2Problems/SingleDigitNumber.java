25f86709d7536a29ad7764830dffcee76acaa3ea
Shashank0606 <shashankrathore606@gmail.com>
2022-11-14 09:37:33 +0530

    Que 1,2

---
+++ Day2Problems/SingleDigitNumber.java
@@ -0,0 +1,28 @@
+package Day2Problems;
+
+public class SingleDigitNumber {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        if (x == 1)
+            System.out.println("One");
+        else if (x == 2)
+            System.out.println("Two");
+        else if (x == 3)
+            System.out.println("Three");
+        else if (x == 4)
+            System.out.println("Four");
+        else if (x == 5)
+            System.out.println("Five");
+        else if (x == 6)
+            System.out.println("Six");
+        else if (x == 7)
+            System.out.println("Seven");
+        else if (x == 8)
+            System.out.println("Eight");
+        else if (x == 9)
+            System.out.println("Nine");
+        else
+            System.out.println("Enter number between 1 to 9");
+    }
+
+}
