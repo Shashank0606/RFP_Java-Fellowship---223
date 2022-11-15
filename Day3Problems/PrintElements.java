d97766335f0933b8f261388d3403885934f0431e
Shashank0606 <shashankrathore606@gmail.com>
2022-11-15 10:25:08 +0530

    que 1

---
+++ Day3Problems/PrintElements.java
@@ -0,0 +1,11 @@
+package Day3Problems;
+
+public class PrintElements {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        System.out.println("Elements of given array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
