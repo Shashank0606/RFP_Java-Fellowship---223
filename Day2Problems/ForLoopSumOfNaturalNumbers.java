0a527037d09380caa8da23920223c99239d2df85
Shashank0606 <shashankrathore606@gmail.com>
2022-11-15 08:29:27 +0530

    que 4,5

--- Day2Problems/ForLoopSumOfNaturalNumbers.java
+++ Day2Problems/ForLoopSumOfNaturalNumbers.java
@@ -1,6 +1,6 @@
 package Day2Problems;
 
-public class SumOfNaturalNumbers {
+public class ForLoopSumOfNaturalNumbers {
     public static void main(String[] args) {
         int num = 5, sum = 0;
         for (int i = 1; i <= num; ++i) {
